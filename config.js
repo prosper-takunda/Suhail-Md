901a701499d42f090f229f8689548ed24a8c5e45
prosper-takunda <168781315+prosper-takunda@users.noreply.github.com>
2024-05-27 00:56:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.video= "" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="saimsamsun789@gmail.com"
-global.location="Lahore,Pakistan."
+global.location="Harare,Zimbabwe."
 
 
 global.mongodb= process.env.MONGODB_URI || ""
